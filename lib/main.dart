e62e77d31b96f960ec6152e24c3b61f473e1e081
Dongwan Kim <zeumes12@gmail.com>
2020-05-25 20:31:41 -0400

    Separate splash page from main.dart and add menu page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'splash.dart';
+import 'menu.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,98 +14,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: SplashScreen(),
+      routes: <String, WidgetBuilder>{
+        '/Menu': (BuildContext context) => new Menu()
+      },
     );
   }
 }
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 5), ()=> print("Splash done!"));
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.yellow[700]),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      /** @TODO: Dongwan will add proper logo here */
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 50.0,
-                        child: Icon(
-                          Icons.sort_by_alpha,
-                          color: Colors.blue[800],
-                          size: 50.0,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10.0),
-                      ),
-                      Text(
-                        "ScrBabel",
-                        style: TextStyle(
-                            fontFamily: 'Pacifico',
-                            color: Colors.white,
-                            fontSize: 24.0,
-                          fontWeight: FontWeight.bold
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(
-                      backgroundColor: Colors.blue[800],
-                      valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue[600]),
-                      strokeWidth: 5,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                    ),
-                    Center(
-                      child: Text(
-                          " \u00a9 copyright \n Josh Sheltzer and Dongwan Kim",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          )
-        ],
-      ) ,
-    );
-  }
-}
-
---
+++ lib/splash.dart
@@ -0,0 +1,107 @@
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  ///  Set 3 seconds for splash page
+  ///Will navigate menu page after 3 seconds
+  startTime() async {
+    var _duration = new Duration(seconds: 3);
+    return new Timer((_duration), navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/Menu');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.yellow[700]),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      /** @TODO: Dongwan will add proper logo here */
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.sort_by_alpha,
+                          color: Colors.blue[800],
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "ScrBabel",
+                        style: TextStyle(
+                            fontFamily: 'Pacifico',
+                            color: Colors.white,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                      backgroundColor: Colors.blue[800],
+                      valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue[600]),
+                      strokeWidth: 5,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Center(
+                      child: Text(
+                        " \u00a9 copyright \n Josh Sheltzer and Dongwan Kim",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ) ,
+    );
+  }
+}
+
